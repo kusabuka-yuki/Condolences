e5ba20b915441a8430ec6b657eb962afb030a6d9
kusabuka-yuki <yk.ksbk0211@gmail.com>
2024-02-24 22:22:28 +0900

    mod

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Condolences;
-// データベースの起動
-
 // アプリケーションの実行
 // クラスの呼出し
 var app = new Condolence();
--- sql/create table.sql
+++ sql/create table.sql
@@ -5,5 +5,5 @@ CREATE TABLE Deceaseds(
 	reason text, 
 	description text ,
 	published_at varchar(255) , 
-	created_at datetime default '1000-01-01 00:00:00'
+	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
